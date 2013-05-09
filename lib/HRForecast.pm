df7f8d7382f0d8cf56ea1e33a9e052cef66449eb
Daisuke Maki <lestrrat+github@gmail.com>
2013-05-09 11:28:26 +0900

    first step after doing some docs and 'minil migrate'

--- lib/HRForecast.pm
+++ lib/HRForecast.pm
@@ -12,3 +12,21 @@ sub config { $CONFIG };
 
 1;
 
+__END__
+
+=head1 NAME
+
+HRForecast - A Graphing/Visualization Tool
+
+=head1 DESCRIPTION
+
+HRForecast is Graphing/Data Visualization tool.  Whereas L<GrowthForecast> (HRForecast's older brother) is a tool for monitoring real-time data, HRForecast is aimed to keep track of data for a relatively long time frame.
+
+So while L<GrowthForecast> shows you data in terms of average rates (i.e. RRD style) and the smallest graphing unit for a given data set is "1min average", HRForecast uses raw numbers and the smallest unit is "value at a given date".
+
+=head1 AUTHOR
+
+Masahiro Nagano C<< <kazeburo {at} gmail.com> >>
+
+
+=cut
